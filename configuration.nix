48b227af881c767caf7572388892d52c82ed3745
Poularde <mattsim@pm.me>
2026-07-25 19:20:28 +0200

    update

--- configuration.nix
+++ configuration.nix
@@ -156,7 +156,8 @@
   programs.appimage = {
     enable = true;
     binfmt = true;
-
+  };
+  
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
