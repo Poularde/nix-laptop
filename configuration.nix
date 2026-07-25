5fdae4ba108b6cebe9b8a519d7c6e84daf51bcd2
Poularde <mattsim@pm.me>
2026-07-25 23:33:40 +0200

    update

--- configuration.nix
+++ configuration.nix
@@ -157,6 +157,12 @@
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
   };
+  pkgs.steam.override = {
+    extraPkgs =
+    pkgs: with pkgs; [            
+      bibata-cursors
+    ];
+  };
 
   # AppImage support
   programs.appimage = {
