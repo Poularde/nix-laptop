05408453fd734bf22c4a73a339d1b650a6111e0e
Poularde <mattsim@pm.me>
2026-07-25 23:40:19 +0200

    update

--- configuration.nix
+++ configuration.nix
@@ -156,12 +156,11 @@
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-  };
-  pkgs.steam.override = {
-    extraPkgs =
-    pkgs: with pkgs; [            
-      bibata-cursors
-    ];
+    package = pkgs.steam.override {
+      extraPkgs = pkgs: with pkgs; [
+        bibata-cursors
+      ];
+    };
   };
 
   # AppImage support
