8395f0a20fadbe2aa6b5b47cabfed302567fa234
Poularde <mattsim@pm.me>
2026-07-26 09:37:10 +0200

    update

--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@
 #eval "$(ssh-agent -s)" && ssh-add ~/.ssh/git
 git add . && git commit -m "update" && git push
 sudo nixos-rebuild switch --upgrade
-sudo nix-collect-garbage --delete-older-than +3
+nix-collect-garbage --delete-older-than +3
