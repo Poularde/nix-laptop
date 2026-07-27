40bfec6a6020c4966e1cd1097389a15f2e8c7814
Poularde <mattsim@pm.me>
2026-07-27 09:19:16 +0200

    update

--- configuration.nix
+++ configuration.nix
@@ -102,7 +102,7 @@
   users.users."poularde" = {
     isNormalUser = true;
     description = "Poularde";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [ "networkmanager" "wheel" "dialout" ];
     shell = pkgs.zsh;
     packages = with pkgs; [
       git
